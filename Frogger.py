d8d394104a13186eb46cb4cc6151980559ef5dd1
Harrmonwuzherrr <Harwat20@yahoo.com>
2014-11-19 16:08:17 -0600

    swgeggduiiohd

--- Frogger.py
+++ Frogger.py
@@ -1,11 +1,149 @@
 from Tkinter import *
 root = Tk()
-
+#Harmon and Vincent
 drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=1)
 grass = drawpad.create_rectangle(0,250,800,600, fill = "green")
-road = drawpad.create_rectangle(0,350,800,500, fill = "gray")
+road = drawpad.create_rectangle(0,340,800,500, fill = "gray")
 water = drawpad.create_rectangle(0,250,800,0, fill = "blue")
+lillypad1 = drawpad.create_rectangle(70,0,110,30,  fill = "DarkGreen")
+lillypad2 = drawpad.create_rectangle(180,0,220,30, fill = "DarkGreen")
+lillypad3 = drawpad.create_rectangle(270,0,310,30, fill = "DarkGreen")
+lillypad4 = drawpad.create_rectangle(390,0,430,30, fill = "DarkGreen")
+lillypad5 = drawpad.create_rectangle(500,0,540,30, fill = "DarkGreen")
+lillypad6 = drawpad.create_rectangle(610,0,650,30, fill = "DarkGreen")
+lillypad7 = drawpad.create_rectangle(720,0,760,30, fill = "DarkGreen")
+truck1 = drawpad.create_rectangle(0,450,60,480, fill = "yellow")
+truck2 = drawpad.create_rectangle(150,450,210,480, fill = "purple")
+truck3 = drawpad.create_rectangle(300,450,360,480, fill = "yellow")
+truck4 = drawpad.create_rectangle(450,450,510,480, fill = "yellow")
+truck5 = drawpad.create_rectangle(600,450,660,480, fill = "purple")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 root.mainloop()
+
+
+
+

fbb3f4adf3eefe9bbbb18bdcb0928fedbb033c28
Harrmonwuzherrr <Harwat20@yahoo.com>
2014-12-04 14:51:33 -0600

    truck you
    get me  a pretzel

--- Frogger.py
+++ Frogger.py
@@ -16,21 +16,20 @@ lillypad7 = drawpad.create_rectangle(720,0,760,30, fill = "DarkGreen")
 #bottom of road
 truck1 = drawpad.create_rectangle(0,450,60,480, fill = "yellow")
 truck2 = drawpad.create_rectangle(150,450,210,480, fill = "purple")
-truck3 = drawpad.create_rectangle(300,450,360,480, fill = "yellow")
-truck4 = drawpad.create_rectangle(450,450,510,480, fill = "yellow")
-
+truck3 = drawpad.create_rectangle(200,350,260,380, fill = "blue")
+truck4 = drawpad.create_rectangle(350,350,410,380, fill = "red")
 #medium row of road
 car1 = drawpad.create_rectangle(40,400,80,430, fill = "red")
 
 # first row of logs
 
-Log4 = drawpad.create_rectangle(450,70,530,30, fill = "brown")
-Log5 = drawpad.create_rectangle(600,70,680,30, fill = "brown")
+log4 = drawpad.create_rectangle(450,70,530,30, fill = "brown")
+log5 = drawpad.create_rectangle(600,70,680,30, fill = "brown")
 #second row of logs
 
-Log8 = drawpad.create_rectangle(300,150,380,190, fill = "brown")
-Log9 = drawpad.create_rectangle(450,150,530,190, fill = "brown")
-Log10= drawpad.create_rectangle(600,150,680,190, fill = "brown")
+log8 = drawpad.create_rectangle(300,150,380,190, fill = "brown")
+log9 = drawpad.create_rectangle(450,150,530,190, fill = "brown")
+log10= drawpad.create_rectangle(600,150,680,190, fill = "brown")
 #middle row of logs
 
 
@@ -38,7 +37,6 @@ Log10= drawpad.create_rectangle(600,150,680,190, fill = "brown")
 player = drawpad.create_oval(350,575,375,600, fill = "lightgreen")
 
 
-collisionlist = [truck1,truck2,truck3,truck4,car1,Log4,Log5,Log8,Log9,Log10,lillypad1,lillypad2,lillypad3,lillypad4,lillypad5,lillypad6,lillypad7,water,player,]
 
 
 

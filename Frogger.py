9d5216a3877dfdbf7c2396ffd6f8c8cd6508a76d
Harrmonwuzherrr <Harwat20@yahoo.com>
2014-12-04 15:43:13 -0600

    LOVE VINCENT AND HARMON

--- Frogger.py
+++ Frogger.py
@@ -23,7 +23,7 @@ car1 = drawpad.create_rectangle(40,400,80,430, fill = "red")
 
 # first row of logs
 
-log4 = drawpad.create_rectangle(450,70,530,30, fill = "brown")
+log4 = drawpad.create_rectangle(450,110,530,70, fill = "brown")
 log5 = drawpad.create_rectangle(600,70,680,30, fill = "brown")
 #second row of logs
 
@@ -32,19 +32,35 @@ log9 = drawpad.create_rectangle(450,150,530,190, fill = "brown")
 log10= drawpad.create_rectangle(600,150,680,190, fill = "brown")
 #middle row of logs
 
-
-
+# i wanna know what love is sssssssssssssssss i want you to show meee yeahhhhehehehehe i want feel what love isssssssssssss i want you to show
 player = drawpad.create_oval(350,575,375,600, fill = "lightgreen")
 
+loglist = [log4,log5,log8,log9,log10]
 
 
 
-
-
-
-
-
+collisionlist = [truck1,truck2,truck3,truck4,car1,water]
+def collisionDect():
+    x1,y1,x2,y2 = drawpad.coords(player)
+    
+    for enemy in enemyList:
+        ex1,ey1,ex2,ey2 = drawpad.coords(enemy)
+        if x1 > ex1 and x1 < ex2 and y1 > ey1 and yi < ey2:
+         drawpad.move(player, 350 - x1, 575 - y1)   
           
+          
+          
+    foundlog = False 
+    if y1 < 800:
+        for log in loglist:
+            lx1,ly1,lx2,ly2 = drawpad.coords(log)
+            if x1 > lx1 and x1 < lx2 and y1 > ly1 and y1 < ly2:
+                drawpad.move(player,logdirection,0)
+                foundlog = True 
+        if foundlog == False:
+            drawpad.move(player, 350 - x1, 575 - y1)
+
+
 
 
 
